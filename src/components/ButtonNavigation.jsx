e86134d6148dd10444290285502c77ef34cfb81e
Eminence <mulemanowa@gmail.com>
2021-05-13 15:24:11 +0200

    refact: modification texte bouton

--- src/components/ButtonNavigation.jsx
+++ src/components/ButtonNavigation.jsx
@@ -5,7 +5,7 @@ function ButtonNavigation({ numPage, nextPage, previousPage, total_pages}) {
         <div className="row bouton-navigation">
             {
 
-                numPage === 1 ? <span onClick={(e) => nextPage(e)} >Page suivant</span> : numPage === total_pages ? <span onClick={(e) => previousPage(e)}>Page précédente</span> : (numPage <= total_pages) ? <><span onClick={(e) => previousPage(e)}>Page précédente</span><span onClick={(e) => nextPage(e)}>Page suivant</span></> : null
+                numPage === 1 ? <span onClick={(e) => nextPage(e)} >Page suivante</span> : numPage === total_pages ? <span onClick={(e) => previousPage(e)}>Page précédente</span> : (numPage <= total_pages) ? <><span onClick={(e) => previousPage(e)}>Page précédente</span><span onClick={(e) => nextPage(e)}>Page suivante</span></> : null
             }
         </div>
     )
