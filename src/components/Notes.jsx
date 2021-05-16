71aff8d241a0baa36bfa9c8ad3b09a8ecc477755
Eminence <mulemanowa@gmail.com>
2021-05-16 11:12:19 +0200

    :art: Feat
    Note average done

---
+++ src/components/Notes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { BsFillStarFill} from 'react-icons/bs'
+function Notes() {
+    return (
+        <BsFillStarFill/>
+    )
+}
+
+export default Notes
--- src/components/sections/CardFilm.jsx
+++ src/components/sections/CardFilm.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { MdPlayArrow, MdAdd } from 'react-icons/md'
 import { FaInfo } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
+import Notes from '../Notes'
 
 function CardFilm({ data, poster_path, genresMovies }) {
+    const Note = [1,2,3,4,5,6,7,8,9,10]
     return (
         
         <div className="col-6 col-md-4 col-lg-2 w-responsive">
@@ -12,10 +14,21 @@ function CardFilm({ data, poster_path, genresMovies }) {
                 <img src={poster_path + data.poster_path} className="card-img-top img-fluid" alt={data.title} />
                 <div className="card-body">
                     <h5 className="card-title fw-bold">{data.title}</h5>
-                    <p className="card-text">
-                        <span >{data.release_date !== undefined ? data.release_date.slice(0, 4) : null}</span>
-                        <span>{data.popularity}</span>
-                        <span>{(data.vote_average * 10) + " %"}</span>
+                    <div className="card-text">
+                        <span className="item" >{data.release_date !== undefined ? data.release_date.slice(0, 4) : null}</span>
+                        <span className="item">{data.popularity}</span>
+                        {/* {(data.vote_average * 10) + " %"} */}
+                        <p>
+                            {
+                                Note.map((note) => (
+                                    note <= Math.trunc(Number(data.vote_average)) ? <span><Notes /></span> : <Notes />
+                             ))
+                           }
+                           
+                        </p>
+                    </div>
+                    <p>
+                       
                     </p>
                 </div>
                 <div className="film-info">
