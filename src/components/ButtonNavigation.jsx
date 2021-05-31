a381dbe5d80e4ef59c2c074c27f7b59572112f7f
Eminence <mulemanowa@gmail.com>
2021-05-31 03:35:01 +0200

    :art: Refact : refactor button to the navigation
    Refact : refactor button to the navigation"

--- src/components/ButtonNavigation.jsx
+++ src/components/ButtonNavigation.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import {FaAngleDoubleLeft,FaAngleDoubleRight} from 'react-icons/fa'
 function ButtonNavigation({ numPage, nextPage, previousPage, total_pages}) {
     return (
         <div className="row bouton-navigation">
             {
 
-                numPage === 1 ? <>{"Page " + numPage + " sur "+total_pages} <span onClick={(e) => nextPage(e)} >Page suivante</span></>  : numPage === total_pages ? <>{"Page " + numPage + " sur "+total_pages}<span onClick={(e) => previousPage(e)}>Page précédente</span></> : (numPage <= total_pages) ? <><span onClick={(e) => previousPage(e)}>Page précédente</span>{"Page " + numPage + " sur "+total_pages}<span onClick={(e) => nextPage(e)}>Page suivante</span></> : null
+                numPage === 1 ? <>{"Page " + numPage + " sur "+total_pages} <span onClick={(e) => nextPage(e)} ><FaAngleDoubleRight/></span></>  : numPage === total_pages ? <>{"Page " + numPage + " sur "+total_pages}<span onClick={(e) => previousPage(e)}><FaAngleDoubleLeft/></span></> : (numPage <= total_pages) ? <><span onClick={(e) => previousPage(e)}><FaAngleDoubleLeft/></span>{"Page " + numPage + " sur "+total_pages}<span onClick={(e) => nextPage(e)}><FaAngleDoubleRight/></span></> : null
             }
         </div>
     )
